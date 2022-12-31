70eaa508298ec5507fd5d9678b1942cfe92005de
Rhythm Munshi <programmingwithrm@gmail.com>
2022-12-31 22:46:24 +0600

    Readme File Edited:1

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../Home/Navbar';
 import loginIng1 from '../../../Img/Login Picture/loginIng1.png'
 import loginIng2 from '../../../Img/Login Picture/loginimg2.png'
@@ -8,6 +8,8 @@ import "keen-slider/keen-slider.min.css"
 import './Login.css'
 import Footer from '../../Home/Foter';
 import { useNavigate } from 'react-router-dom';
+import AOS  from 'aos';
+import 'aos/dist/aos.css';
 const Login = () => {
     return (
         <div>
@@ -64,18 +66,25 @@ function LoginComponent() {
           },
         ]
       )
+      useEffect(() => {
+        AOS.init({
+            offset: 100,
+            duration: 1000,
+            easing: 'ease'
+        });
+    })
     return (
         <div className="container LoginComponent">
             <div className="row d-flex justify-content-center align-items-center">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 SBox">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 SBox" data-aos="fade-right">
                     <h1 className='Wlc'>Welcome to the new world of learning</h1>
                     <form onSubmit={handleSubmit}>
                         <input type="email" placeholder="Enter Email" required title='Enter Email' /> <br />
                         <button style={{ padding: '10px 30px', borderRadius: '30px' }} className='buttons'>Submit</button>
                     </form>
                 </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 ImgBoxLogin">
-                    <h1 className='Wlc' style={{margin:'5px 15px'}}>Participate in daily live classes and keep yourself engaged</h1>
+                <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 ImgBoxLogin "data-aos="fade-left">
+                    {/* <h1 className='Wlc' style={{margin:'5px 15px'}}>Participate in daily live classes and keep yourself engaged</h1> */}
                 <div ref={sliderRef} className="keen-slider">
                     <div className="keen-slider__slide number-slide1 manageH">
                         <img className='manageH' src={loginIng1} alt="" />
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css'
 import Himg from '../../Img/frt-img.png'
 import Sicon from '../../Img/Icon/Speak.png'
@@ -8,11 +8,19 @@ import Data from '../Data/Data';
 import YtClass from '../Data/PVClassData';
 import Footer from './Foter';
 import Navbar from './Navbar';
-
+import AOS  from 'aos';
+import 'aos/dist/aos.css';
 // import "./styles.css";
 
 
 const Home = () => {
+      useEffect(() => {
+        AOS.init({
+            offset: 100,
+            duration: 1000,
+            easing: 'ease'
+        });
+    })
     return (
         <div className="home">
             <Front></Front>
@@ -35,11 +43,18 @@ function Front(){
 
 
 function Header(){
+  useEffect(() => {
+    AOS.init({
+        offset: 100,
+        duration: 1000,
+        easing: 'ease'
+    });
+})
     return(
       <div className="header">
         <div className="container">
         <div className="row CoverUp">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 Hcol">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 Hcol" data-aos="fade-right">
               {/* <img className="Bimg" src={BImg} alt="" /> */}
                 <h1 className='title'>Up Your <span style={{color: '#7F56D9',}}>Skills</span> <br/> To <span style={{color: '#7F56D9',}}>Advance</span> Your <br/> <span style={{color: '#7F56D9',}}>Career</span> Path</h1>
                 <p className='smt'>Provides you with the latest online learning system and material that help your knowledge growing.</p>
@@ -62,7 +77,7 @@ function Header(){
                 </div>
                 </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6" data-aos="fade-left">
                 <img className='imgH img-fluid' src={Himg} alt="" />
             </div>
         </div>
@@ -72,10 +87,17 @@ function Header(){
 }
 
 function Services(){
+  useEffect(() => {
+    AOS.init({
+        offset: 100,
+        duration: 1000,
+        easing: 'ease'
+    });
+})
   return(
     <div className='container service'>
-      <h5 style={{color: '#6941C6',fontWeight:'600',fontSize:'18px'}}>Our Services</h5>
-      <h1 className='serviceT' style={{color: '#101828',}}>Fostering a playful & engaging learning environment</h1>
+      <h5 data-aos="fade-up" style={{color: '#6941C6',fontWeight:'600',fontSize:'18px'}}>Our Services</h5>
+      <h1 data-aos="fade-up" className='serviceT' style={{color: '#101828',}}>Fostering a playful & engaging learning environment</h1>
       <div className="row Sbox">
       {
         Data.map(value => <Swipe img={value.img} name={value.name} description={value.description}></Swipe>)
@@ -86,8 +108,15 @@ function Services(){
 }
 
 function Swipe(props){
+  useEffect(() => {
+    AOS.init({
+        offset: 100,
+        duration: 1000,
+        easing: 'ease'
+    });
+})
   return(
-    <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+    <div data-aos="fade-up" className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
      <div class="Cbox">
             <img className='cImg' src={props.img} alt="" />
               <h4 className="cH">{props.name}</h4>
@@ -99,9 +128,16 @@ function Swipe(props){
 }
 
 function PClass(){
+  useEffect(() => {
+    AOS.init({
+        offset: 100,
+        duration: 1000,
+        easing: 'ease'
+    });
+})
   return(
     <div className='container PClass'>
-      <div className="Cheading">
+      <div data-aos="fade-right" className="Cheading">
         <h5 style={{color: '#6941C6',fontWeight:'600',fontSize:'18px',marginTop:'30px'}}>Explore Programs </h5>
         <h1 className='ClassT' style={{color: '#101828',}}>Our Most Popular Class</h1>
         <p className='smt'>Let's join our famous class, the knowledge provided will definitely be useful for you.</p>
@@ -119,8 +155,15 @@ function PClass(){
 }
 
 function YtRow(props){
+  useEffect(() => {
+    AOS.init({
+        offset: 100,
+        duration: 1000,
+        easing: 'ease'
+    });
+})
   return(
-    <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+    <div data-aos="fade-up" className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
       <div style={{textAlign: 'center'}} className="YTRCard">
       <iframe className='Classbox' src={props.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <h4 className="CH">{props.name}</h4>
