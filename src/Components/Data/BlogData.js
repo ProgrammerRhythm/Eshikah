ddb2ffc1db3bc0303ff9ecea955626e88d27363c
Rhythm Munshi <programmingwithrm@gmail.com>
2023-01-15 16:34:40 +0600

    Up: Services Sec api:2

---
+++ src/Components/Data/BlogData.js
@@ -0,0 +1,3 @@
+const Blogs= [
+    {title: 'The Blockchain Buzz',img: '',content: '',about: '',date: ''}
+];
--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -1,11 +1,8 @@
-import img1 from '../../Img/SImg/1st.png'
-import img2 from '../../Img/SImg/Frame 391 (1).png'
-import img3 from '../../Img/SImg/Frame 391.png'
 
 const Data = [
-    {id:'A',name: 'User Experience',description: 'Lessons on design that cover the most recent developments.',img:{img1}},
-    {id:'B',name: 'Web Development',description: 'Classes in development that cover the most recent in web.',img:{img2}},
-    {id:'C',name: 'Marketing',description: 'Marketing courses that the most recent marketing trends',img:{img3}}
+    {id:'A',name: 'User Experience',description: 'Lessons on design that cover the most recent developments.',img:'https://www.linkpicture.com/q/1st_3.png'},
+    {id:'B',name: 'Web Development',description: 'Classes in development that cover the most recent in web.',img:'https://www.linkpicture.com/q/Frame-391-1.png'},
+    {id:'C',name: 'Marketing',description: 'Marketing courses that the most recent marketing trends',img:'https://www.linkpicture.com/q/Frame-391.png'}
 ]
 
 export default Data;
