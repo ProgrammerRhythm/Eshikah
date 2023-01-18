d8cfcc9564fb35aee44f93e1e35c6ef2ae120d9f
Tanzil Islam <tanzil.ncpsc@gmail.com>
2023-01-18 23:56:41 +0600

    refactor: seperate backend source code to different branch

--- frontend/src/Components/Data/Data.js
+++
@@ -1,8 +0,0 @@
-
-const Data = [
-    {id:'A',name: 'User Experience',description: 'Lessons on design that cover the most recent developments.',img:'https://www.linkpicture.com/q/1st_3.png'},
-    {id:'B',name: 'Web Development',description: 'Classes in development that cover the most recent in web.',img:'https://www.linkpicture.com/q/Frame-391-1.png'},
-    {id:'C',name: 'Marketing',description: 'Marketing courses that the most recent marketing trends',img:'https://www.linkpicture.com/q/Frame-391.png'}
-]
-
-export default Data;
