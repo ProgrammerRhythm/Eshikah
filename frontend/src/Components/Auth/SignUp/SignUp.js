74b5e2498ba47e1d968bd747b524390b1e733a8a
Rhythm Munshi <programmingwithrm@gmail.com>
2023-01-22 00:32:10 +0600

    SignIn/SignUp Token decoded:1

--- frontend/src/Components/Auth/SignUp/SignUp.js
+++ frontend/src/Components/Auth/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../../Home/Navbar';
 // import SignUpIng from '../../../Img/login.webp'
 import './SignUp.css'
@@ -10,7 +10,10 @@ import loginIng1 from '../../../Img/Login Picture/loginIng1.png'
 import loginIng2 from '../../../Img/Login Picture/loginimg2.png'
 import loginIng3 from '../../../Img/Login Picture/loginIng3.png'
 import { useKeenSlider } from 'keen-slider/react';
+import jwt_decode from "jwt-decode";
 import Aos from 'aos';
+import { UserContext } from '../../../App';
+import { useNavigate } from 'react-router-dom';
 // const notify = () => toast('Here is your toast.');
 
 const SignUp = () => {
@@ -25,6 +28,8 @@ const SignUp = () => {
 
 
 function SignUpComponent(){
+  const [logedInUser,setLoggedInUser] = useContext(UserContext);
+  console.log(logedInUser);
     // const history = useNavigate()
     // const handleSubmit = (e) => {
     //     history('/dashboard/info')
@@ -74,69 +79,76 @@ function SignUpComponent(){
             easing: 'ease'
         });
     })
-    const [user,setUser] = useState({
+    const [user,setNewUser] = useState({
       firstName: '',
       lastName: '',
     })
-    const [token,setToken] = useState({
-      token: ''
-    })
+    // const [token,setToken] = useState({
+    //   token: ''
+    // })
     const HandleChange = (e) => {
-      let fildValid = true;
-      if(e.target.name === 'fName') {
+      let fildValid=true;
+      if(e.target.name === 'firstName') {
           fildValid =  e.target.value;
       }
-      if(e.target.name === 'lName') {
+      if(e.target.name === 'lastName') {
         fildValid =  e.target.value;
     }
       if (fildValid) {
           const newUserInfo = {...user};
           newUserInfo[e.target.name]=e.target.value;
-          setUser(newUserInfo);
+          setNewUser(newUserInfo);
+          console.log(newUserInfo);
       }
     }
     const OnSubmit = () => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const token = queryParams.get('token');
-    console.log(token);
-      const firstName = user.firstName;
-      const lastName = user.lastName;
-      const date = new Date().getTime();
-      const totalValue = {
-          "firstName": firstName,
-          'lastName': lastName,
-          'dateOfBirth': date,
-          'token': token
-      }
-      console.log(totalValue);
-      console.log(JSON.stringify(totalValue));
-      SendData(totalValue);
-      }
-      function SendData(value) {
-        fetch('https://eshika.lazytanzil.repl.co/api/auth/register',{
-        method: 'POST',
-        body: JSON.stringify(value),
-        headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-      },
-      })
-      .then(res => res.json())
-      .then(result => {
-        const token = result.token;
-         const uToken = {token: token}
-         setToken(uToken);
-         console.log(uToken,result);
-      })
-      .catch(err => console.log(err))
-      }
+      const queryParams = new URLSearchParams(window.location.search);
+      const token = queryParams.get('token');
+      console.log(token);
+        const firstName = user.firstName;
+        const lastName = user.lastName;
+        const date = new Date().getTime();
+        const totalValue = {
+            "firstName": firstName,
+            'lastName': lastName,
+            'dateOfBirth': date,
+            'token': token
+        }
+        console.log(totalValue);
+        console.log(JSON.stringify(totalValue));
+        SendData(totalValue);
+        }
+        const history = useNavigate();
+        function SendData(value) {
+          fetch('https://eshika.lazytanzil.repl.co/api/auth/register',{
+          method: 'POST',
+          body: JSON.stringify(value),
+          headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then(res => res.json())
+        .then(result => {
+          
+          const token = result.token;
+          const UserData = jwt_decode(token);
+          console.log(UserData);
+          const {email,lastName,firstName,} = UserData;
+          const signedInUser = {name:`${firstName} ${lastName}`, email: email}
+          setLoggedInUser(signedInUser);
+          history('/dashboard')
+        })
+        .catch(err => console.log(err))
+        }
+        
     return (
         <div className="container LoginComponent">
             <div className="row d-flex justify-content-center align-items-center">
                 <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 SBox" data-aos="fade-right">
                     <h1 className='Wlc'>Welcome to the new world of learning</h1>
-                      <h5 style={{fontSize:'15px',color:'red'}}>{token.token}</h5>
-                        <input onChange={HandleChange} className='inputF' name='fName' type="text"  placeholder="Enter your First Name" required title='Enter your First Name' /> <br />
-                        <input  onChange={HandleChange} placeholder='Enter your Last Name' name='lname' className='inputF' type="text"  required  id="" />
+                      {/* <h5 style={{fontSize:'15px',color:'red'}}>{token.token}</h5> */}
+                        <input onChange={HandleChange} className='inputF' name='firstName' type="text"  placeholder="Enter your First Name" required title='Enter your first Name' /> <br />
+                        <input  onChange={HandleChange} placeholder='Enter your Last Name' name='lastName' className='inputF' type="text"  required title='Enter your last Name'  id="" />
                         <br />
                         <button style={{ padding: '10px 30px', borderRadius: '30px' }} onClick={() => OnSubmit()} id="click" className='buttons'>Submit</button>
                 </div>
