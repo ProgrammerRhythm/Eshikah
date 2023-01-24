04a54cf918ebd8f33350d1a1b0dc617569d6afab
Rhythm Munshi <programmingwithrm@gmail.com>
2023-01-25 01:01:47 +0600

    User Data Saved in Localstroge:1

--- frontend/src/Components/Program/PrivateRoute.js
+++ frontend/src/Components/Program/PrivateRoute.js
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { UserContext } from '../../App';
+// import { UserContext } from '../../App';
 
 const PrivateRoute = ({ children }) => {
     const location = useLocation()
-    const [logedInUser] = useContext(UserContext)
-    return logedInUser.email ? children : <Navigate  to={{
+    const jsonUser = localStorage.getItem('user');
+    const Luser = JSON.parse(jsonUser)
+    // const [logedInUser] = useContext(UserContext)
+    return Luser.email ? children : <Navigate  to={{
         pathname: "/auth",
         state: { from: location }
       }} />;
